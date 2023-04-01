2104d63f67f213a67fd9365b41fd2b7485f6da74
GrungeElFz <elf0114_@hotmail.com>
2023-04-01 15:33:49 -0500

    Initialized:   Cloned from `git@github.com:GrungeElFz/2023.03.28_Svelte.10.git` repository

---
+++ src/lib/pages/Home.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Button from '../Button.svelte';
+	import { Field, Form } from '../Form';
+	import { validateRequiredField, validateEmail } from '../utils/validation';
+</script>
+
+<Form
+	on:submit={(e) => {
+		console.log(e.detail);
+	}}
+	initialValues={{ username: 'Test', email: 'test@test.com' }}
+>
+	<Field label="Username" name="username" type="text" validate={validateRequiredField} />
+	<Field
+		label="Email"
+		name="email"
+		type="email"
+		validate={(value, label) => {
+			return validateRequiredField(value, label) || validateEmail(value, label);
+		}}
+	/>
+	<Field label="Password" name="password" type="password" validate={validateRequiredField} />
+	<Button type="submit">Submit</Button>
+</Form>
