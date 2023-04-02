4e9d15594575f64ce9f80f68bceedd70cf8fe431
GrungeElFz <elf0114_@hotmail.com>
2023-04-02 18:13:28 -0500

    Modified:   Duplicated a <Layer> component (src/lib/pages/Home.svelte)             Duplicated a <Rect> component
                In the first <Rect> component
                - Changed a `y` attribute value from 40 to 20
                - Changed a `fill` attribute value from "purple" to "MediumSlateBlue"
                - Changed a `width` attribute value from 100 to 200
    
                In the second <Rect> component
                - Changed a `x` attribute value from 20 to 50
                - Changed a `y` attribute value from 40 to 50
                - Changed a `fill` attribute value from "purple" to "DeepSkyBlue"
    
                In the third <Rect> component
                - Changed a `x` attribute value from 20 to 80
                - Changed a `y` attribute value from 40 to 80
                - Changed a `fill` attribute value from "purple" to "gold"

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -9,7 +9,12 @@
 {#if showCanvas}
 	<Stage width={300} height={400}>
 		<Layer>
-			<Rect x={20} y={40} fill="purple" width={100} height={100} />
+			<Rect x={20} y={20} fill="MediumSlateBlue" width={200} height={100} />
+			<Rect x={50} y={50} fill="DeepSkyBlue" width={100} height={100} />
+		</Layer>
+
+		<Layer>
+			<Rect x={80} y={80} fill="gold" width={100} height={100} />
 		</Layer>
 	</Stage>
 {/if}
