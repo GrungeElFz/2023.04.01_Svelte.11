69fd26ddd6b81e2c3ddd49cc9702c61519da92ab
GrungeElFz <elf0114_@hotmail.com>
2023-04-02 01:08:18 -0500

    Modified:   import { onDestroy, onMount } from 'svelte' (src/lib/konva/Stage.svelte)             import Konva from 'konva'
                export let width
                export let height
    
    	          Declared a let variable `container`
    	          Declared a let variable `stage`
    
                Invoked `onMount()`
                Invoked `onDestroy()`
    
                Added a <div bind:this={container}> element
                Added a nested <slot> compoent in the div

---
+++ src/lib/konva/Stage.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { onDestroy, onMount } from 'svelte';
+	import Konva from 'konva';
+
+	export let width;
+	export let height;
+
+	let container;
+	let stage;
+
+	onMount(() => {
+		stage = new Konva.Stage({
+			container,
+			width,
+			height
+		});
+	});
+
+	onDestroy(() => {
+		if (stage) stage.destroy();
+	});
+</script>
+
+<div bind:this={container}>
+	<slot />
+</div>
