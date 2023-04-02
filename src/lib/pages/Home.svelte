8734d475eafc4e567cfecfd2b9a02384fce546ec
GrungeElFz <elf0114_@hotmail.com>
2023-04-02 01:12:02 -0500

    Modified:   Assigned a let variable `showCanvas = true` (src/lib/pages/Home.svelte)             Added an <input type="checkbox" bind:checked={showCanvas} /> element
                Added an {#if showCanvas} block
                Wrapped a <Stage> component with the block

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,9 +1,15 @@
 <script>
 	import { Stage, Layer, Rect } from '../konva';
+
+	let showCanvas = true;
 </script>
 
-<Stage width={300} height={400}>
-	<Layer>
-		<Rect x="20" y="40" fill="purple" width="100" height={100} />
-	</Layer>
-</Stage>
+<input type="checkbox" bind:checked={showCanvas} />
+
+{#if showCanvas}
+	<Stage width={300} height={400}>
+		<Layer>
+			<Rect x="20" y="40" fill="purple" width="100" height={100} />
+		</Layer>
+	</Stage>
+{/if}
