3045146f00ca25f05c131b4a5fcf3b1c64e3279f
GrungeElFz <elf0114_@hotmail.com>
2023-04-02 00:41:13 -0500

    Modified:   Cleared the entire file (src/lib/pages/Home.svelte)
                import { Stage, Layer, Rect } from '../konva'
    
                Added <Stage> component
                Added <Layer> component
                Added <Rect> component

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,43 +1,9 @@
 <script>
-	import { onMount } from 'svelte';
-	import Konva from 'konva';
-
-	let container;
-
-	onMount(() => {
-		const stage = new Konva.Stage({
-			container,
-			width: 300,
-			height: 400
-		});
-
-		const layer = new Konva.Layer();
-
-		const rect1 = new Konva.Rect({
-			x: 20,
-			y: 20,
-			width: 100,
-			height: 60,
-			fill: 'purple',
-			stroke: 'white',
-			strokeWidth: 4
-		});
-
-		const rect2 = new Konva.Rect({
-			x: 150,
-			y: 20,
-			width: 100,
-			height: 100,
-			fill: 'aqua',
-			shadowColor: 'rgba(1,1,1,0.5)',
-			shadowBlur: 10,
-			cornerRadius: 5
-		});
-
-		layer.add(rect1);
-		layer.add(rect2);
-		stage.add(layer);
-	});
+	import { Stage, Layer, Rect } from '../konva';
 </script>
 
-<div bind:this={container} />
+<Stage width={300} height={400}>
+	<Layer>
+		<Rect x="20" y="40" fill="purple" width="100" height={100} />
+	</Layer>
+</Stage>
