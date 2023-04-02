c3a012d4d386d2dc91edb863ea29dcc7a3827e76
GrungeElFz <elf0114_@hotmail.com>
2023-04-02 01:56:43 -0500

    Modified:   Fixed a value type from `""` by `{}` for 3 attributes in <Rect> component (src/lib/pages/Home.svelte)

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -9,7 +9,7 @@
 {#if showCanvas}
 	<Stage width={300} height={400}>
 		<Layer>
-			<Rect x="20" y="40" fill="purple" width="100" height={100} />
+			<Rect x={20} y={40} fill="purple" width={100} height={100} />
 		</Layer>
 	</Stage>
 {/if}
